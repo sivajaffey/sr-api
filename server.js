49bb0f189c5904e314c35bdbc59d4ef2eb0b0a46
snarayanan2 <snarayanan2@lenovo.com>
2019-11-13 19:10:12 +0530

    mysql connection setup in server.js

--- server.js
+++ server.js
@@ -2,14 +2,38 @@ var express = require('express');
 var app = express();
 var host = 'localhost'
 var port = 3000;
+// mysql connection
+var mysql = require('mysql');
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: ""
+  });
+  
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    con.query("CREATE DATABASE srdb", function (err, result) {
+        console.log("Database created");
+    });
+  });
 
 app.get('/', function (req, res) {
    res.send('Hello World');
 })
 app.post('/question', function (req, res) {
-    console.log(req['query']);
     let data = req['query'];
-    res.send(data);
+    let query = {
+       'question' : data.question,
+       'tags' : JSON.stringify(data.tags),
+       'user_id' : data.user_id
+    }
+    let sql = `insert into question_table(question,tag,user_id) values('${query.question}',${query.tags},'${query.user_id}')`;
+    console.log(sql)
+    con.query(sql,function(res,result){
+      console.log('success')
+    });  
+    res.send(query);
  })
 
 var server = app.listen(8081, function () {
